bd0feb29c408675725f35ec16fd6c3418ec107a4
Gretchen Maculo <gretchynm@gmail.com>
2021-03-12 11:27:39 -0600

    .

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
-import setuptools
-setuptools.setup(scripts=['bin/icat'])
+from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='icat',
+    version='0.0.16',
+    license='GPL3',
+    url='https://github.com/gretchycat/icat',
+    author='Gretchen Maculo',
+    author_email='gretchen.maculo@gmail.com',
+    description='print images on an ansi capable terminal',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'Pillow',
+    ],
+    tests_require=[
+    ],
+    scripts=['icat/icat']
+)
