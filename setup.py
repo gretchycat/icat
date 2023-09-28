776107cd938bdf14b81ebda3a30f966381aafea3
Gretchen Maculo <gretchynm@gmail.com>
2023-09-27 21:48:42 -0500

    Correctly copying videos on select

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ shutil.copyfile('icat/__main__.py', 'icat/icat')
 
 setup(
     name='icat',
-    version='0.0.33',
+    version='0.0.34',
     license='GPL3',
     url='https://github.com/gretchycat/icat',
     author='Gretchen Maculo',
