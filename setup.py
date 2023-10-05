37877788eca55ce426a8c8233a4a8b13964d5802
Gretchen Maculo <gretchynm@gmail.com>
2023-10-05 14:18:35 -0500

    crash fixes in api mode

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ shutil.copyfile('icat/__main__.py', 'icat/icat')
 
 setup(
     name='icat',
-    version='0.0.35',
+    version='0.0.36',
     license='GPL3',
     url='https://github.com/gretchycat/icat',
     author='Gretchen Maculo',
